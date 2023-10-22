ea81cb392dbe01cb0eb69255106888090c5399cb
Tane4ka170 <tane4ka170@gmail.com>
2023-10-22 11:37:23 +0300

    uptade

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
-export const App = () => {
-  return (
-    <div>
+import React from "react";
+import { nanoid } from 'nanoid';
 
-    </div>
-  );
-};
+class App extends React.Component{
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState([name], value)
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const { name, value } = event.target;
+    const { contact, addContact } = this.props;
+    if (contects.some((contact) => contact.name === name)) {
+      alert(`This ${name} is already in contacts`);
+      return
+    }
+  }
+}
